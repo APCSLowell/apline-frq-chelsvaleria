d6629be74f55b8ee94697bf93d5d20cae8a87b80
chelsvaleria <143123776+chelsvaleria@users.noreply.github.com>
2024-04-22 19:54:17 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -15,7 +15,7 @@ public void setA(int newA) {myA = newA;}
 public void setB(int newB) {myB = newB;}
 public void setC (int newC) {myC = newC;}
 public int getSlope(){
-return -(double)myA/(double)myB;
+return -(double)myA / (double)myB;
 }
 public boolean isOnLine(int x, int y){
 return (myA * x) + (myB * y) + myC == 0;
