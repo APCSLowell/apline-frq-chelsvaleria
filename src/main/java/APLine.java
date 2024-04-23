a34b91e20d571713bdca75af3f2edcdda24488e3
chelsvaleria <143123776+chelsvaleria@users.noreply.github.com>
2024-04-22 19:47:17 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,29 @@
 public class APLine
 {
-  /* your code here */
+private int myA; 
+private int myB;
+private int myC;
+public APline( int a, int b, int c){
+myA = a;
+myB = b;
+my C = c;
+}
+public int getA(){return myA;}
+public int getB(){return myB;}
+public int getC() {return myC;}
+public void setA(int newA) {myA = newA;}
+public void setB(int newB) {myB = newB;}
+public void setC (int newC) {myC = newC;}
+public int getSlope(){
+double ans = (double) -myA/(double)myB;
+return ans;
+}
+public boolean isOnLine(int x, int y){
+boolean onLine = false;
+int num = (myA*x)+(myB*y)+(myC);
+if(num == 0)
+onLine = true;
+return onLine;
+}
   
 }
